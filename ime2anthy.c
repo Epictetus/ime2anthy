d52b7cfef5d1cb67cb0cc0a3216f43a9854a8b6e
Masami Ichikawa <masami256@gmail.com>
2009-10-04 14:10:13 +0900

    Use utf8 instead of euc.

--- ime2anthy.c
+++ ime2anthy.c
@@ -8,11 +8,10 @@
 
 #define NKF_OUTPUT_FILE_NAME "ime_std_utf.txt"
 #define ANTHY_DIC_FILE_NAME "/.anthy/2ch_kaomoji.txt"
-#define ANTHY_DIC_TMP_FILE_NAME "/.anthy/2ch_kaomoji.txt.tmp"
 
 static void usage(char *prog);
 static void do_command(char *com);
-static void create_anthy_dic_template(char *file, char *tmp);
+static void create_anthy_dic_template(char *file);
 static void do_nkf(char *in, char *in_code, char *out, char *out_code);
 static void convert_imedic2anthydic(char *in, char *out);
 static void load_dictionary(char *file);
@@ -22,7 +21,7 @@ static void load_dictionary(char *file)
 	char cmd[512] = { 0 };
 
 	snprintf(cmd, sizeof(cmd) - 1,
-		 "cat %s | anthy-dic-tool --load | nkf -E", file);
+		 "cat %s | anthy-dic-tool --load --utf8", file);
 
 	do_command(cmd);
 
@@ -74,14 +73,12 @@ static void do_command(char *com)
 	assert(system(com) != -1);
 }
 
-static void create_anthy_dic_template(char *file, char *tmp)
+static void create_anthy_dic_template(char *file)
 {
 	char com[512] = { 0 };
 
-	snprintf(com, sizeof(com) - 1, "anthy-dic-tool --dump > %s", file);
+	snprintf(com, sizeof(com) - 1, "anthy-dic-tool --dump --utf8 > %s", file);
 	do_command(com);
-
-	do_nkf(file, "E", tmp, "w8");
 }
 
 static void do_nkf(char *in, char *in_code, char *out, char *out_code)
@@ -101,25 +98,20 @@ static void usage(char *prog)
 int main(int argc, char **argv)
 {
 	char anthy_dic[512] = { 0 };
-	char anthy_dic_tmp[512] = { 0 };
 
 	if (argc != 2)
 		usage(argv[0]);
 
 	snprintf(anthy_dic, sizeof(anthy_dic) - 1, "%s/%s", getenv("HOME"), ANTHY_DIC_FILE_NAME);
-	snprintf(anthy_dic_tmp, sizeof(anthy_dic_tmp) - 1, "%s/%s", getenv("HOME"), ANTHY_DIC_TMP_FILE_NAME);
 
-	create_anthy_dic_template(anthy_dic, anthy_dic_tmp);
+	create_anthy_dic_template(anthy_dic);
 	do_nkf(argv[1], "S", NKF_OUTPUT_FILE_NAME, "w8");
 
-	convert_imedic2anthydic(NKF_OUTPUT_FILE_NAME, anthy_dic_tmp);
-
-	do_nkf(anthy_dic_tmp, "W8", anthy_dic, "e");
+	convert_imedic2anthydic(NKF_OUTPUT_FILE_NAME, anthy_dic);
 
 	load_dictionary(anthy_dic);
 
 	g_remove(NKF_OUTPUT_FILE_NAME);
-	g_remove(anthy_dic_tmp);
 
 	return 0;
 }
