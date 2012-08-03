1a3744c187e66601829646ac5831c29008c85ea2
Masami Ichikawa <masami256@gmail.com>
2012-08-03 23:43:25 +0900

    fix indent

--- ime2anthy.rb
+++ ime2anthy.rb
@@ -7,34 +7,34 @@ require 'nkf'
 DICT_FILE_NAME = ENV["HOME"] + "/.anthy/2ch_kaomoji.txt"
 
 def load_dict_file(flg)
-    cmd = ""
+	cmd = ""
 
-    if !flg then
-	    cmd = sprintf "cat %s | anthy-dic-tool --load --utf8", DICT_FILE_NAME
-    else
-	    cmd = sprintf "cat %s | anthy-dic-tool --load --utf8 --append", DICT_FILE_NAME
-    end
+	if !flg then
+		cmd = sprintf "cat %s | anthy-dic-tool --load --utf8", DICT_FILE_NAME
+	else
+		cmd = sprintf "cat %s | anthy-dic-tool --load --utf8 --append", DICT_FILE_NAME
+	end
 	system(cmd)
 end
 
 def create_dict_data(text)
 	tmp = text.split(/\s*\t\s*/)
-    if tmp.size == 3
-        if tmp[0].index("ヴ") != nil then 
-            tmp[0] = tmp[0].gsub("ヴ", "ぼ")
-        elsif tmp[0] == "・・・" then
-            tmp[0] = "てんてんてん"
-        end
-    end
+	if tmp.size == 3
+		if tmp[0].index("ヴ") != nil then 
+			tmp[0] = tmp[0].gsub("ヴ", "ぼ")
+		elsif tmp[0] == "・・・" then
+			tmp[0] = "てんてんてん"
+		end
+	end
 
-    return tmp 
+	return tmp 
 end
 
 def write_dict_file(data)
 	f = open(DICT_FILE_NAME, "w")
 
 	data.each { |d|
-        arr = create_dict_data(d)
+		arr = create_dict_data(d)
 		s = sprintf "%s 1 %s\n%s\n%s\n%s\n%s\n%s\n%s\n\n",
 			NKF::nkf( '-WwXm0', arr[0]),
 			arr[1],
@@ -65,16 +65,16 @@ def check_file(name)
 end
 
 if __FILE__ == $0
-    if ARGV.size == 0 then 
-        printf "usage: %s [dict file]\n", $0
-        exit 0
-    end
-    f = ARGV[0]
+	if ARGV.size == 0 then 
+		printf "usage: %s [dict file]\n", $0
+		exit 0
+	end
+	f = ARGV[0]
 
-    append_flg = false
-    if ARGV.size == 2 then
-        append_flg = true
-    end
+	append_flg = false
+	if ARGV.size == 2 then
+		append_flg = true
+	end
 
 	if ! check_file(f) then
 		printf "file %s is not found\n", f
